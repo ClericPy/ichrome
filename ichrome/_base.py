8553b4e239ea0c83f08cbc7d20842dbacd25e868
clericpy <lidongone@qq.com>
2018-11-23 02:43:10 +0800

    partial job

--- ichrome/_base.py
+++ ichrome/_base.py
@@ -4,9 +4,14 @@ from torequests import tPool
 import threading
 from torequests.utils import print_info
 import os
+import json
 import subprocess
 import socket
 import time
+import traceback
+from queue import Queue
+import websocket
+from collections import deque
 
 
 def mute_log():
@@ -56,7 +61,7 @@ class ChromeDaemon(object):
         start_url="about:blank",
         extra_config=None,
         auto_restart=True,
-        max_deaths=3,
+        max_deaths=2,
     ):
         self.proc = None
         self.chrome_path = self._ensure_chrome(chrome_path or self.DEFAULT_CHROME_PATH)
@@ -112,12 +117,10 @@ class ChromeDaemon(object):
             try:
                 sock = socket.socket()
                 sock.connect((self.host, self.port))
-                if self.req.get(self.server + "/json", timeout=2).x.ok:
-                    print_info(
-                        "shutting down another chrome instance using port %s"
-                        % self.port
-                    )
-                self.kill()
+                print_info(
+                    "shutting down another chrome instance using port %s" % self.port
+                )
+                self.kill(True)
                 continue
             except ConnectionRefusedError:
                 return True
@@ -186,50 +189,61 @@ class ChromeDaemon(object):
         return False
 
     def daemon(self, interval=5, max_deaths=None):
-        """if be kill 3 times too fast (not raise TimeoutExpired),
-        will skip the auto_restart logic."""
+        """if chrome proc is killed 3 times too fast (not raise TimeoutExpired),
+        will skip auto_restart."""
         return_code = None
         deaths = 0
         max_deaths = max_deaths or self.max_deaths
-        while deaths < max_deaths:
+        while 1:
             try:
                 return_code = self.proc.wait(timeout=interval)
                 deaths += 1
             except subprocess.TimeoutExpired:
                 deaths = 0
+            if deaths >= max_deaths:
+                break
             if not self.alive and self.auto_restart:
                 self.restart()
                 continue
         else:
             print_info("exit daemon")
 
-    def run_forever(self):
+    def run_forever(self, block=True):
         t = threading.Thread(target=self.daemon, daemon=True)
         t.start()
-        t.join()
+        if block:
+            t.join()
 
-    def kill(self):
-        print_info("shut down %s." % self)
+    def kill(self, force=False, cycle=5):
         self.ready = False
         if self.proc:
             self.proc.kill()
-        self.clear_chrome_process(self.port)
+        if force:
+            self.clear_chrome_process(self.port, timeout=5)
+        else:
+            self.clear_chrome_process(self.port)
         self.port_in_using.add(self.port)
 
     def restart(self):
         self.kill()
         self.launch_chrome()
 
+    def shutdown(self):
+        print_info("shut down %s." % self)
+        self.auto_restart = False
+        self.kill()
+
     @staticmethod
-    def clear_chrome_process(port=None, cycle=1, interval=2):
+    def clear_chrome_process(port=None, timeout=None):
         """kill chrome processes, if port is not set, kill all chrome with --remote-debugging-port
-        set `cycle = 3 (>=max_deaths)` make auto_restart lose efficacy.
+        set timeout to ensure chrome not restart (to make auto_restart lose efficacy).
         """
         port = port or ""
         # win32 and linux chrome proc_names
         proc_names = {"chrome.exe", "chrome"}
         port_args = "--remote-debugging-port=%s" % port
-        for _ in range(cycle):
+        start_time = time.time()
+        while 1:
             for proc in psutil.process_iter():
                 try:
                     pname = proc.name()
@@ -240,54 +254,54 @@ class ChromeDaemon(object):
                                 proc.kill()
                 except:
                     pass
-            if cycle > 1:
-                time.sleep(interval)
+            if timeout and time.time() - start_time < timeout:
+                time.sleep(1)
+                continue
+            return
 
     def __del__(self):
-        print_info("deleting %s." % self)
-        self.kill()
+        print_info("%s.__del__()." % self)
+        self.shutdown()
 
     def __str__(self):
         return "%s(%s:%s)" % (self.__class__.__name__, self.host, self.port)
 
 
 class Chrome(object):
-    def __init__(self, host="localhost", port=9222):
-        self._host = host
-        self._port = port
-        self._url = "http://%s:%d" % (self.host, self.port)
-        self._errmsg = (
-            "Connect error! Is Chrome running with -remote-debugging-port=%d"
-            % self.port
-        )
-        self._connect()
+    def __init__(self, host="localhost", port=9222, timeout=2, retry=1):
+        self.req = tPool()
+        self.host = host
+        self.port = port
+        self.timeout = timeout
+        self.retry = retry
+        if not self.ok:
+            raise IOError("Can not connect to %s" % self.server)
 
-    def _connect(self):
+    @property
+    def ok(self):
         """
         Test connection to browser
         """
-        try:
-            requests.get(self.url, timeout=3)
-        except RequestException:
-            raise RequestException(self._errmsg)
+        r = self.req.get(self.server, timeout=self.timeout, retry=self.retry)
+        if r.ok:
+            return True
+        return False
 
     def _get_tabs(self):
         """
         Get all open browser tabs that are pages tabs
         """
         try:
-            r = req.get(self.url + "/json", timeout=2, retry=2)
-            # print(self.url + "/json")
-            # print(r.x)
-            # print(r.json(), 111111111)
+            r = self.req.get(
+                self.server + "/json", timeout=self.timeout, retry=self.retry
+            )
             return [
-                ChromeTab(
+                Tab(
                     tab["id"],
                     tab["title"],
                     tab["url"],
                     tab["webSocketDebuggerUrl"],
-                    self.host,
-                    self.port,
+                    self,
                 )
                 for tab in r.json()
                 if tab["type"] == "page"
@@ -297,39 +311,180 @@ class Chrome(object):
             return []
 
     @property
-    def host(self):
-        return self._host
-
-    @property
-    def port(self):
-        return self._port
-
-    @property
-    def url(self):
-        return self._url
+    def server(self):
+        return "http://%s:%d" % (self.host, self.port)
 
     @property
     def tabs(self):
-        return tuple(self._get_tabs())
-
-    def __len__(self):
-        return len(self.tabs)
+        return self._get_tabs()
 
     def __str__(self):
-        return "[Chromote(tabs=%d)]" % len(self)
+        return "[Chromote(tabs=%d)]" % len(self.tabs)
 
     def __repr__(self):
-        return 'Chromote(host="%s", port=%s)' % (self.host, self.port)
-
-    def __getitem__(self, i):
-        return self.tabs[i]
-
-    def __iter__(self):
-        return iter(self.tabs)
+        return "Chromote(%s)" % (self.server)
 
 
 class Tab(object):
-    pass
+    def __init__(self, tid, title, url, websocketURL, chrome, timeout=None):
+        self.tid = tid
+        self.title = title
+        self.url = url
+        self.websocketURL = websocketURL
+        self.chrome = chrome
+        self.timeout = timeout
+
+        self._message_id = 0
+        self._watchers = []
+        self.lock = threading.Lock()
+        self.ws = websocket.WebSocket()
+        self._connect()
+        for job in [self._recv_daemon, self._queue_watcher]:
+            t = threading.Thread(target=self._recv_daemon, daemon=True)
+            t.start()
+
+    def _connect(self):
+        self.ws.connect(self.websocketURL, timeout=self.timeout)
+
+    def _recv_daemon(self):
+        """
+        {"id":1,"result":{}}
+        {"id":3,"result":{"result":{"type":"string","value":"http://p.3.cn/"}}}
+        {"id":2,"result":{"frameId":"7F34509F1831E6F29351784861615D1C","loaderId":"F4BD3CBE619185B514F0F42B0CBCCFA1"}}
+        {"method":"Page.frameStartedLoading","params":{"frameId":"7F34509F1831E6F29351784861615D1C"}}
+        {"method":"Page.frameNavigated","params":{"frame":{"id":"7F34509F1831E6F29351784861615D1C","loaderId":"F4BD3CBE619185B514F0F42B0CBCCFA1","url":"http://p.3.cn/","securityOrigin":"http://p.3.cn","mimeType":"application/json"}}}
+        {"method":"Page.loadEventFired","params":{"timestamp":120277.621681}}
+        {"method":"Page.frameStoppedLoading","params":{"frameId":"7F34509F1831E6F29351784861615D1C"}}
+        {"method":"Page.domContentEventFired","params":{"timestamp":120277.623606}}
+        """
+        while self.ws.connected:
+            try:
+                data = self.ws.recv()
+                data = json.loads(data)
+                for item in self._watchers:
+                    # TODO 等实现接收器, 并且要清理掉 _watcher
+                    if item[0] in data.items():
+                        item[1].send(data)
+
+                # self._message_queue.put({"ts": int(time.time()), "msg": self.ws.recv()})
+            except websocket._exceptions.WebSocketConnectionClosedException:
+                break
+
+    def _recv(self, arg, timeout=None):
+        # TODO 这里注册了事件以后, 要监听 Timeout 时间内如果触发, 拿到返回值
+        timeout = timeout or self.timeout
+        # 等待超时的一个接收器, 要么超时, 要么提前收到结果, 考虑生成器或者队列
+        # 
+        listener = None
+        arg = tuple(arg.items())[0]
+        self._watchers.append([arg, listener])
+
+    def _send(self, request):
+        for _ in range(3):
+            try:
+                self._message_id += 1
+                request["id"] = self._message_id
+                with self.lock:
+                    self.ws.send(json.dumps(request))
+                res = self.recv({"id": request["id"]})
+                return res
+            except (
+                websocket._exceptions.WebSocketTimeoutException,
+                websocket._exceptions.WebSocketConnectionClosedException,
+            ):
+                self.refresh_ws()
+
+    def refresh_ws(self):
+        self.ws.close()
+        self._connect()
+
+    @property
+    def now(self):
+        return int(time.time())
+
+    def clear_cookies(self):
+        return self._send({"method": "Network.clearBrowserCookies"})
+
+    @property
+    def html(self):
+        try:
+            result = json.loads(self.js("document.documentElement.outerHTML"))
+            value = result["result"]["result"]["value"]
+            return value.encode("utf-8")
+        except KeyError:
+            print_info(traceback.format_exc())
+            return ""
+
+    def _wait_loading(self):
+        data = self._wait_event("Page.loadEventFired")
+        return data
+
+    def _wait_event(self, event=None):
+        # block
+        timeout = 6
+        start_time = time.time()
+        ws = ws or self.ws
+        data = None
+        while time.time() - start_time < timeout:
+            try:
+                data = ws.recv()
+                if not data:
+                    continue
+                data = json.loads(data)
+                if not event or data.get("method") == event:
+                    return data
+            except (
+                websocket._exceptions.WebSocketTimeoutException,
+                websocket._exceptions.WebSocketConnectionClosedException,
+            ):
+                return None
+            # except Exception as e:
+            # print('wait event %s failed for %s' % (event, e))
+
+    def reload(self, timeout=5):
+        """
+        Reload the page
+        """
+        return self.set_url(timeout=timeout)
+
+    def set_url(self, url=None, timeout=5):
+        """
+        Navigate the tab to the URL, 
+        """
+        ws = self.connect(timeout)
+        self._send({"method": "Page.enable"})
+        if url:
+            self.url = url
+            data = self._send({"method": "Page.navigate", "params": {"url": url}})
+        else:
+            data = self._send({"method": "Page.reload"})
+        if self._wait_loading(ws=ws) is None:
+            self._send({"method": "Page.stopLoading"})
+        ws.close()
+        return data
+
+    def js(self, javascript):
+        """
+        Evaluate JavaScript on the page
+        """
+        return self._send(
+            {"method": "Runtime.evaluate", "params": {"expression": javascript}}
+        )
+
+    def __str__(self):
+        return "Tab(%s)" % (self.url)
+
+    def __repr__(self):
+        return 'ChromeTab("%s", "%s", "%s", port: %s)' % (
+            self.tid,
+            self.title,
+            self.url,
+            self.chrome.port,
+        )
+
+    def __del__(self):
+        with self.lock:
+            self.ws.close()
 
 
 def main():
@@ -337,8 +492,9 @@ def main():
     # # Launcher
     # # time.sleep(3000)
     # chrome.run_forever()
-    # chrome = Chrome()
-    ChromeDaemon.clear_chrome_process(cycle=3)
+    chrome = Chrome()
+    tab = chrome.tabs[0]
+    print(tab)
 
 
 if __name__ == "__main__":
--- test.py
+++ test.py
@@ -1,8 +1,66 @@
 # import aiohttp
+import websocket
+from threading import Thread
+from queue import Queue
+import time
 
 
-# sock = socket.socket()
-# sock.connect(('localhost', 8081))
-# sock.close()
+# # q = Queue()
 
-from ichrome import Chrome
+# # def test(ws):
+# #     while ws.connected:
+# #         try:
+# #             print(ws.recv())
+# #         except websocket._exceptions.WebSocketConnectionClosedException:
+# #             break
+# #     print('stop')
+
+# # url = "ws://localhost:9222/devtools/page/7F34509F1831E6F29351784861615D1C"
+# # ws = websocket.WebSocket()
+# # ws.connect(url)
+# # t = Thread(target=test, args=[ws], daemon=1)
+# # t.start()
+# # ws.send('{"method": "Page.enable", "id": 1}')
+# # ws.send('{"method": "Page.navigate", "params": {"url": "http://p.3.cn"}, "id": 2}')
+# # ws.send('{"method": "Runtime.evaluate", "params": {"expression": "window.location.href"}, "id": 3}')
+# # time.sleep(.1)
+# # ws.close()
+# import traceback
+
+
+def test():
+    start_time = time.time()
+    while time.time()- start_time< 2:
+        # time.sleep(1)
+        result = yield "还没有"
+        
+        print('result:', result)
+        if result:
+            time.sleep(1)
+            return result
+    print('超时了')
+    return
+
+
+g = test()
+# print(next(g))
+g.send(None)
+
+# g.send('ssdf')
+for i in g:
+    # time.sleep(1)
+    print(i)
+#     try:
+#         print(next(g))
+#     except StopIteration:
+#         break
+#     if i > 1:
+#         try:
+#             print(g.send('有了'), 1111)
+#         except StopIteration as e:
+#             print(1222, e.value)
+# # print(next(g))
+# print(next(g))
+# print(next(g))
+# print(next(g))
+# print(next(g))
