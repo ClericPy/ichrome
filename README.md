30d9e14f12603952801736c1f00adffef4576b36
clericpy <lidongone@qq.com>
2018-11-24 00:49:20 +0800

    main features finished

--- README.md
+++ README.md
@@ -1,1 +1,72 @@
 # ichrome
+
+> A toy for using chrome under the "Chrome Devtools Protocol(CDP)". For python3.6+ (who care python2.x).
+
+## Features
+
+- Chrome process daemon
+- Connect to existing chrome debug port
+- Operations on Tabs
+
+## Examples
+
+### Chrome daemon
+
+```python
+from ichrome import ChromeDaemon
+
+def main():
+    chrome = ChromeDaemon()
+    chrome.run_forever()
+
+if __name__ == "__main__":
+    main()
+```
+
+### Connect to existing debug port
+
+```python
+from ichrome import Chrome
+
+def main():
+    chrome = Chrome()
+    print(chrome.tabs)
+    # [ChromeTab("6EC65C9051697342082642D6615ECDC0", "about:blank", "about:blank", port: 9222)]
+    print(chrome.tabs[0])
+    # Tab(about:blank)
+
+if __name__ == "__main__":
+    main()
+```
+
+### Operations on Tab
+
+```python
+from ichrome import Chrome
+
+import time
+
+
+def main():
+    chrome = Chrome()
+    print(chrome.tabs)
+    # [ChromeTab("6EC65C9051697342082642D6615ECDC0", "about:blank", "about:blank", port: 9222)]
+    tab = chrome.tabs[0]
+    # open a new page
+    print(tab.set_url("http://p.3.cn/1", timeout=3))  # {"id":4,"result":{}}
+    # reload page
+    print(tab.reload())  # {"id":4,"result":{}}
+    # Not recommended new_tab with url, use set_url can set a timeout to stop loading
+    # tab = chrome.new_tab()
+    # tab.set_url("http://p.3.cn", timeout=3)
+    tab = chrome.new_tab("http://p.3.cn/new")
+    time.sleep(1)
+    print("404 Not Found" in tab.get_html("u8"))  # True
+    print(tab.current_url)  # http://p.3.cn/new
+    tab.close()
+
+
+if __name__ == "__main__":
+    main()
+
+```
--- test.py
+++ test.py
@@ -1,39 +1,26 @@
-# import aiohttp
-import websocket
-from threading import Thread
-from queue import Queue
+from ichrome import Chrome
+
 import time
 
 
-# # q = Queue()
+def main():
+    chrome = Chrome()
+    print(chrome.tabs)
+    # [ChromeTab("6EC65C9051697342082642D6615ECDC0", "about:blank", "about:blank", port: 9222)]
+    tab = chrome.tabs[0]
+    # open a new page
+    print(tab.set_url("http://p.3.cn/1", timeout=3))  # {"id":4,"result":{}}
+    # reload page
+    print(tab.reload())  # {"id":4,"result":{}}
+    # Not recommended new_tab with url, use set_url can set a timeout to stop loading
+    # tab = chrome.new_tab()
+    # tab.set_url("http://p.3.cn", timeout=3)
+    tab = chrome.new_tab("http://p.3.cn/new")
+    time.sleep(1)
+    print("404 Not Found" in tab.get_html("u8"))  # True
+    print(tab.current_url)  # http://p.3.cn/new
+    tab.close()
 
-# # def test(ws):
-# #     while ws.connected:
-# #         try:
-# #             print(ws.recv())
-# #         except websocket._exceptions.WebSocketConnectionClosedException:
-# #             break
-# #     print('stop')
 
-# # url = "ws://localhost:9222/devtools/page/7F34509F1831E6F29351784861615D1C"
-# # ws = websocket.WebSocket()
-# # ws.connect(url)
-# # t = Thread(target=test, args=[ws], daemon=1)
-# # t.start()
-# # ws.send('{"method": "Page.enable", "id": 1}')
-# # ws.send('{"method": "Page.navigate", "params": {"url": "http://p.3.cn"}, "id": 2}')
-# # ws.send('{"method": "Runtime.evaluate", "params": {"expression": "window.location.href"}, "id": 3}')
-# # time.sleep(.1)
-# # ws.close()
-# import traceback
-
-qq = Queue(1)
-import traceback
-qq.a = 32
-print(qq.a)
-
-try:
-    qq.get(timeout=1)
-except Exception as e:
-    traceback.print_exc()
-    print(e)
+if __name__ == "__main__":
+    main()
