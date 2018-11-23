52c8b1d1ef195c705c2c05c30903db85aca7f32e
clericpy <lidongone@qq.com>
2018-11-24 01:03:00 +0800

    update examples

--- test.py
+++ test.py
@@ -1,26 +1,17 @@
-from ichrome import Chrome
+from ichrome import Chrome, Tab, ChromeDaemon
 
 import time
 
 
-def main():
-    chrome = Chrome()
-    print(chrome.tabs)
-    # [ChromeTab("6EC65C9051697342082642D6615ECDC0", "about:blank", "about:blank", port: 9222)]
-    tab = chrome.tabs[0]
-    # open a new page
-    print(tab.set_url("http://p.3.cn/1", timeout=3))  # {"id":4,"result":{}}
-    # reload page
-    print(tab.reload())  # {"id":4,"result":{}}
-    # Not recommended new_tab with url, use set_url can set a timeout to stop loading
-    # tab = chrome.new_tab()
-    # tab.set_url("http://p.3.cn", timeout=3)
-    tab = chrome.new_tab("http://p.3.cn/new")
-    time.sleep(1)
-    print("404 Not Found" in tab.get_html("u8"))  # True
-    print(tab.current_url)  # http://p.3.cn/new
-    tab.close()
+def example():
+    with ChromeDaemon() as chromed:
+        chromed.run_forever(0)
+        chrome = Chrome()
+        tab = chrome.new_tab()
+        time.sleep(3)
+        tab.close()
+        
 
 
 if __name__ == "__main__":
-    main()
+    example()
