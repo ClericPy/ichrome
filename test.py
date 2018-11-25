ce26a3b2b05e36c8f10b28bfaf7634a9404249e8
clericpy <lidongone@qq.com>
2018-11-26 02:08:17 +0800

    use register future instead of queue; add filter for wait_event

--- test.py
+++ test.py
@@ -3,21 +3,31 @@ from ichrome import Chrome, Tab, ChromeDaemon
 import time
 import json
 from torequests.utils import print_info
+import re
 
 
 def example2():
+    from ichrome import ichrome_logger
+    import logging
+
+    # ichrome_logger.setLevel(logging.DEBUG)
     with ChromeDaemon() as chromed:
         chrome = Chrome()
-        tab = chrome.new_tab()
-        tab.set_url("http://baidu.com")
-        print(tab.send("Network.enable"))
-        recv_string = tab.recv({"method": "Network.responseReceived"})
+        tab = chrome.new_tab("http://p.3.cn")
+        tab.set_url("https://www.bing.com/")
+        print_info(tab.send("Network.enable"))
+        # here will block until input `test` in the baidu input position.
+        recv_string = tab.wait_event(
+            "Network.responseReceived",
+            filter_function=lambda r: re.search("&\w+=test", r or ""),
+            wait_seconds=9999,
+        )
         recv_string = json.loads(recv_string)
         rid = recv_string["params"]["requestId"]
-        tab.send(
-            "Network.getResponseBody", requestId=rid, callback=lambda r: print(r, "getResponseBody ok")
-        )
-        chromed.run_forever()
+        print_info("requestId: %s" % rid)
+        a = tab.send("Network.getResponseBody", requestId=rid, timeout=5)
+        print_info(a, "getResponseBody success")
+        # chromed.run_forever()
 
 
 def example():
@@ -32,5 +42,5 @@ def example():
 
 
 if __name__ == "__main__":
-    #     example()
+    # example()
     example2()
