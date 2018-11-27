23f1417b2f01c3ff0647fb952c85491b3cc19abb
clericpy <lidongone@qq.com>
2018-11-27 23:11:24 +0800

    update python3 limit for setup

--- setup.py
+++ setup.py
@@ -4,11 +4,12 @@ import codecs
 
 """
 linux:
-rm -rf "dist/*";rm -rf "build/*";python3 setup.py bdist_wheel;python2 setup.py bdist_wheel;twine upload "dist/*;rm -rf "dist/*";rm -rf "build/*""
+rm -rf "dist/*";rm -rf "build/*";python3 setup.py bdist_wheel;twine upload "dist/*;rm -rf "dist/*";rm -rf "build/*""
 win32:
-rm -rf dist&rm -rf build&python3 setup.py bdist_wheel&python2 setup.py bdist_wheel&twine upload "dist/*"&rm -rf dist&rm -rf build
+rm -rf dist&rm -rf build&python3 setup.py bdist_wheel&twine upload "dist/*"&rm -rf dist&rm -rf build
 """
-
+if sys.version_info < (3, 6):
+    sys.exit("pypinfo requires Python 3.6+")
 py_version = sys.version_info
 install_requires = ["psutil", "torequests", "websocket-client"]
 with open("README.md", encoding="utf-8") as f:
@@ -29,4 +30,9 @@ setup(
     url="https://github.com/ClericPy/ichrome",
     packages=find_packages(),
     platforms="any",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+    ],
 )
