9cb9fb0a601e0508814aaf14ffe456665887465e
clericpy <lidongone@qq.com>
2019-10-03 00:35:49 +0800

    finish http Chrome

--- temp.py
+++ temp.py
@@ -5,7 +5,22 @@ import asyncio
 async def test():
     chrome = Chrome()
     assert await chrome.connect()
-    print(await chrome.tabs())
+    assert await chrome.get_version()
+    assert await chrome.version
+    assert await chrome.new_tab()
+    tabs = await chrome.tabs
+    assert tabs
+    tab = tabs[0]
+    assert tab.title == 'about:blank'
+    assert await tab.refresh()
+    await asyncio.sleep(1)
+    assert await tab.activate_tab()
+
+
+    await asyncio.sleep(1)
+    assert await tab.close_tab()
+    # assert await chrome.close_tabs()
+
 
 
 if __name__ == "__main__":
