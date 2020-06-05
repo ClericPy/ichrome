63168d7f1233f6c882c0566700f45bbd73e87762
clericpy <clericpy@gmail.com>
2020-06-06 01:30:51 +0800

    upgrade 2.1.2

--- ichrome/__init__.py
+++ ichrome/__init__.py
@@ -5,7 +5,7 @@ from .daemon import AsyncChromeDaemon, ChromeDaemon, ChromeWorkers
 from .logs import logger
 from .sync_utils import Chrome, Tab
 
-__version__ = "2.1.1"
+__version__ = "2.1.2"
 __tips__ = "[github]: https://github.com/ClericPy/ichrome\n[cdp]: https://chromedevtools.github.io/devtools-protocol/\n[cmd args]: https://peter.sh/experiments/chromium-command-line-switches/"
 __all__ = [
     'Chrome', 'ChromeDaemon', 'Tab', 'Tag', 'AsyncChrome', 'AsyncTab', 'logger',
