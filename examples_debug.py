ab157e5b2b83b4dcb61ab36d67c0cb4af96f9c45
clericpy <clericpy@gmail.com>
2020-06-23 22:32:02 +0800

    update debugger examples

--- examples_debug.py
+++ examples_debug.py
@@ -1,7 +1,7 @@
 import time
 
 from ichrome import AsyncTab
-from ichrome.debugger import *
+from ichrome.debugger import get_a_tab, network_sniffer
 
 # There are 3 ways to create a daemon
 # 1. get_a_tab: auto find the existing Chrome (launched before like python -m ichrome), or create a new daemon
@@ -21,8 +21,7 @@ def test_set_ua():
 
 def test_mouse_keyboard():
     # click the input element and send some string
-    tab.set_url('http://httpbin.org/forms/post')
-    tab.wait_loading(5)
+    tab.set_url('http://httpbin.org/forms/post', timeout=3)
     rect: dict = tab.get_bounding_client_rect('[type="email"]')
     tab.mouse_click(rect['left'], rect['top'], count=1)
     tab.keyboard_send(string='123@1.com')
@@ -38,15 +37,24 @@ def test_js():
     print(tag, tag.text)
     img_b64 = tab.screenshot_element('.shader_left')
     tab.set_html(
-        f'<h1>Here is the screenshot image by css selector ".shader_left"</h1><img src="data:image/png;base64, {img_b64}" alt="Red dot" /><h1>Script will shutdown in 5 seconds</h1>'
+        f'<h1>Here is the screenshot image by css selector ".shader_left"</h1><img src="data:image/png;base64, {img_b64}" alt="Red dot" /><h1>Test js finished</h1>'
     )
-    time.sleep(5)
 
 
-if __name__ == "__main__":
+def main():
     test_set_ua()
     test_mouse_keyboard()
     test_js()
+    for _ in range(3):
+        tab.set_html(
+            f'<h1>Start network_sniffer in %s seconds. Refresh this page and read network logs in terminal.</h1>'
+            % (3 - _))
+        time.sleep(1)
+    network_sniffer()
     # explicitly stop daemon is not necessary, daemon launched by debugger will auto shutdown after this script quit
     # if the daemon is not launched by this script (using an existing one), the auto-shutdown will not run.
     # daemon.stop()
+
+
+if __name__ == "__main__":
+    main()
