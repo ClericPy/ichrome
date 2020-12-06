7b8990dd8596b09f1733029ccdbc2538b5f08ba9
clericpy <clericpy@gmail.com>
2020-12-06 10:46:30 +0800

    fix '<' not supported between instances of 'NoneType' and 'Process'

--- ichrome/base.py
+++ ichrome/base.py
@@ -85,7 +85,7 @@ def get_proc_by_regex(regex, proc_names=None):
             if (not proc_names or proc.name() in proc_names) and re.search(
                     regex, ' '.join(proc.cmdline())):
                 procs.append(proc)
-        except (psutil.Error, OSError):
+        except (psutil.Error, OSError, TypeError):
             pass
     return procs
 
