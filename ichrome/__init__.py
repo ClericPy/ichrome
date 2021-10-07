9ac1a63e9e53780ca5019148add3e86804f61320
clericpy <clericpy@gmail.com>
2021-10-07 10:40:58 +0800

    wait_event forever bug

--- ichrome/__init__.py
+++ ichrome/__init__.py
@@ -7,7 +7,7 @@ from .logs import logger
 from .pool import ChromeEngine
 from .sync_utils import Chrome, Tab
 
-__version__ = "2.8.3"
+__version__ = "2.8.4"
 __tips__ = "[github]: https://github.com/ClericPy/ichrome\n[cdp]: https://chromedevtools.github.io/devtools-protocol/\n[cmd args]: https://peter.sh/experiments/chromium-command-line-switches/"
 __all__ = [
     'Chrome', 'ChromeDaemon', 'Tab', 'Tag', 'AsyncChrome', 'AsyncTab', 'logger',
